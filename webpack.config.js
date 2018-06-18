041f766dbb8be3a819d6fb58dfd11dd4ece2e67a
cmdallas <self@chrisdallas.tech>
2018-06-17 20:46:35 -0700

    update webpack config/packages

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,33 @@ module.exports = {
   entry: [
     './js/app.js'
   ],
-  mode: 'development',
   output: {
     path: __dirname + '/static',
-    filename: "bundle.js"
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.js?$|\.jsx?$/,
+        test: /\.jsx?$/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015', 'react']
         },
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [ 'file-loader' ]
       }
     ]
   },
   plugins: [
   ],
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.css']
   }
 };
